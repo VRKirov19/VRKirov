16c77e38c48a2042d9341f92706c06489ded07c2
Виктор Руменов Киров <VRKirov19@codingburgas.bg>
2022-10-19 13:16:47 +0300

    Car and Lamp

---
+++ src/Lamp.java
@@ -0,0 +1,14 @@
+public class Lamp {
+    private int light;
+
+    public void BlueYellow (String command){
+        if(command == "Yellow")
+        {
+            System.out.println("Lamp is yellow");
+        }
+        if(command == "Blue")
+        {
+            System.out.println("Lamp is blue");
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Lamp lamp = new Lamp();
+
+        String light = "Blue";
+
+
+        lamp.BlueYellow(light);
+    };
+}
