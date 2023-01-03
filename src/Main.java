3075b0c4367c368b0669f9b8850436aa45c308dc
Виктор Руменов Киров <VRKirov19@codingburgas.bg>
2023-01-03 09:21:26 +0200

    1/3/2023

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
-import java.sql.SQLOutput;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Cat cat = new Cat(5, 3.5);
-        Dog dog = new Dog(10,12.5,"German Sheppard");
-        List<Animal> animals = new ArrayList<>();
-        animals.add(cat);
-        animals.add(dog);
-        System.out.println(dog);
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String[] tokens = input.split("\\s+");
+        for (int i = 0; i < tokens.length/2; i++) {
+            String temp = tokens[i];
+            tokens[i] = tokens[tokens.length - 1 - i];
+            tokens[tokens.length - 1 - i] = temp;
+        }
+
+        System.out.println(String.join(" ", tokens));
     }
 }
