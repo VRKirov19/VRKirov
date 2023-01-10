93065194d6adcf2bda204bfcd926de899002f3b5
Виктор Руменов Киров <VRKirov19@codingburgas.bg>
2023-01-10 09:19:14 +0200

    1/10/2023

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,41 @@
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<>();
+        List<String> linked = new LinkedList<>();
+        List<String> strings = new LinkedList<>();
+
+        strings.add("second");
+        strings.add("adsa");
+        strings.add("third");
+
+        System.out.println(String.join(", ", strings));
+        Collections.sort(strings);
+        System.out.println(String.join(", ", strings));
+        Collections.reverse(strings);
+        System.out.println(String.join(", ", strings));
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+
+        nums = Arrays.stream(input.split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
+
         String[] tokens = input.split("\\s+");
-        for (int i = 0; i < tokens.length/2; i++) {
-            String temp = tokens[i];
-            tokens[i] = tokens[tokens.length - 1 - i];
-            tokens[tokens.length - 1 - i] = temp;
+
+//        System.out.println(tokens[1]);
+//        tokens[1] = "Index1";
+//        System.out.println(tokens[1]);
+
+        for (int i = 0; i < tokens.length; i++) {
+            int element = Integer.parseInt(tokens[i]);
+            nums.add(element);
         }
 
-        System.out.println(String.join(" ", tokens));
+        for (int i = 0; i < nums.size(); i++) {
+            System.out.print(nums.get(i) + " ");
+            nums.set(i, 10);
+        }
     }
 }
