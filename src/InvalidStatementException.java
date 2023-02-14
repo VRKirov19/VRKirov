aaed3df2a8719a145127d577fdbe112ffa43856f
Виктор Руменов Киров <VRKirov19@codingburgas.bg>
2023-02-14 09:13:31 +0200

    14/2/2023

---
+++ src/InvalidStatementException.java
@@ -0,0 +1,7 @@
+package com.pluralsight.calcengine;
+
+public class InvalidStatementException extends Exception {
+    public InvalidStatementException(String message) {
+        super(message);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,17 @@
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
-        Deque<String> stack = new ArrayDeque<>();
-      Scanner scanner = new Scanner(System.in);
-      String input = scanner.nextLine();
-      while(!input.equals("print")) {
-          ArrayDeque<Object> queue;
-          if(input.equals("cancel")){
-              if(queue.isEmpty()) {
-                  System.out.println("Printer is on stand by");
-              }
-              else{
-                  System.out.println("Canceled" + queue.poll());
-              }
-              else{
-                  queue.offer(input);
-              }
-              input = scanner.nextLine();
-          }
-          while(!queue.isEmpty()){
-              System.out.println(queue.poll());
-          }
-      }
+        try {
+            int i = 12;
+            int j = 0;
+            int result = i/j;
+            System.out.println(result);
+        } catch (NumberFormatException ex){
+            System.out.println(ex.getMessage() + "!!!");
+        } catch (ArithmeticException ex){
+            System.out.println(ex.getMessage() + "...");
+        } catch (Exception ex){
+            System.out.println(ex.getMessage() + ",,,,,");
+        }
+        System.out.println("test");
     }
 }
